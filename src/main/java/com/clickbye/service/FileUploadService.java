5e46399c6b8d850d1eedf752adde338d38d9cf10
QuizMJD <darkscopio2@mail.com>
2025-03-23 17:04:09 +0700

    #2

--- src/main/java/com/clickbye/service/FileUploadService.java
+++
@@ -1,8 +0,0 @@
-package com.clickbye.service;
-
-import com.clickbye.dto.FileUploadDTO;
-import java.util.Map;
-
-public interface FileUploadService {
-    Map<String, String> uploadFiles(FileUploadDTO fileUploadDTO);
-} 
